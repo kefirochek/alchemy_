473ac6f9ab3ecdd805ca8dd306b31aef7f0d34ae
kefirochek <honortv11@gmail.com>
2025-04-14 16:30:33 +0300

    ну че не так

--- capitan/data/users.py
+++ capitan/data/users.py
@@ -5,15 +5,23 @@ from sqlalchemy import orm
 
 
 class User(SqlAlchemyBase):
-    __tablename__ = 'users'
+    def __init__(self):
+        __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String,
-                              index=True, unique=True, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
-    jobs = orm.relationship("Jobs", back_populates='user')
+        surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+        name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+        age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+        position = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+        speciality = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+        address = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+        email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+        hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+        modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+        jobs = orm.relationship("Jobs", back_populates='user')
+        self.id = id
+        self.name = name
+        self.surname = surname
+        self.adress = address
+
+    def __repr__(self):
+        return f'<Colonist> {self.id} {self.name} {self.surname}'
